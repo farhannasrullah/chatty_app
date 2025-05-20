80ddbc97248d56771c0b13ccbaceff857ad8a9e8
Farhan Nasrullah <farhannasrullah3@gmail.com>
2025-05-20 15:45:40 +0700

    Update theme_provider.dart

---
+++ lib/Themes/theme_provider.dart
@@ -0,0 +1,22 @@
+import 'package:chatty_app/Themes/Dark_Mode.dart';
+import 'package:chatty_app/Themes/Light_Mode.dart';
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeData _themeData = lightmode;
+  ThemeData get themeData => _themeData;
+
+  bool get isDarkMode => _themeData == darkMode; 
+  set ThemeData(ThemeData themeData) {
+    _themeData = themeData;
+    notifyListeners();
+  }
+
+  void toggleTheme() {
+    if (_themeData == lightmode) {
+      _themeData = darkMode;
+    } else {
+      _themeData = lightmode;
+    } 
+  }
+}
