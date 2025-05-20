bb88c3fd826d82662927ac27af73bd33633a3536
Farhan Nasrullah <farhannasrullah3@gmail.com>
2025-05-20 16:20:11 +0700

    Dark Mode part2

---
+++ lib/Themes/Dark_Mode.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+  colorScheme: ColorScheme.dark(
+    background: Colors.grey.shade900,
+    primary: Colors.grey.shade700,
+    secondary: const Color.fromARGB(255, 57, 57, 57),
+    tertiary: Colors.white,
+    inversePrimary: Colors.grey.shade900,
+  ),
+);
